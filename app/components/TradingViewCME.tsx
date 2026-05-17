1a02d2fbd7bc2230c852141d7db0b503fc158beb
BNextDigital <info@bnextdigitalmarketing.ca>
2026-05-17 16:42:40 -0400

    Update TradingViewCME.tsx

--- app/components/TradingViewCME.tsx
+++ app/components/TradingViewCME.tsx
@@ -1,9 +1,9 @@
 export default function TradingViewCME() {
   return (
-    <div className="rounded-2xl overflow-hidden border border-[#22231F] bg-[#131315]">
+    <div className="overflow-hidden border border-[#22231F] bg-[#131315]" style={{ height: "500px" }}>
       <iframe
-        src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_cme&symbol=CME%3ABTC1%21&interval=D&hidesidetoolbar=1&symboledit=1&saveimage=0&toolbarbg=131315&studies=%5B%5D&theme=dark&style=1&timezone=America%2FNew_York&withdateranges=1&hideideas=1"
-        style={{ width: "100%", height: "500px" }}
+        src="https://sslcharts.investing.com/index.php?force_lang=56&pair_ID=961747"
+        style={{ width: "100%", height: "100%" }}
         title="CME Bitcoin Futures"
       />
     </div>
